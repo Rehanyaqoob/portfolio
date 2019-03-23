9ceb372261ad047bb0a7cacb763f9cd6272f9cbc
rehan.vs <engr.rehan786@hotmail.com>
2019-03-24 02:36:08 +0500

    remove bccrypt pkg

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
 
-module.exports.encrpt = function (text){
-    return bcrypt.hashSync(text, parseInt(process.env.SALT_ROUND));
-}
+// module.exports.encrpt = function (text){
+//     return bcrypt.hashSync(text, parseInt(process.env.SALT_ROUND));
+// }
